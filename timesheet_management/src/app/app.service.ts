5cd84f7ad6d987c3ce101d10d5c28d755cdbea1d
MohamadFazuan <fazuanboy@gmail.com>
2023-12-07 22:37:01 +0800

    added: CRUD services

--- timesheet_management/src/app/app.service.ts
+++ timesheet_management/src/app/app.service.ts
@@ -1,28 +1,32 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environment/environment.dev';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor(private http: HttpClientModule) { }
+  constructor(private http: HttpClient) { }
 
-  create = () => {
-
+  create = (body: JSON) => {
+    return this.http.post(environment.URL_ADDDRESS + '/create', body);
   }
 
   readAll = () => {
-
+    return this.http.get(environment.URL_ADDDRESS + '/readAll');
   }
 
-  readItem = () => {
-
+  readItem = (body: JSON) => {
+    return this.http.post(environment.URL_ADDDRESS + '/readItem', body);
   }
 
-  updateItem = () =>{
-
+  updateItem = (body: JSON) => {
+    return this.http.post(environment.URL_ADDDRESS + '/updateItem', body);
   }
 
-  
+  delete = (body: JSON) => {
+    return this.http.delete(environment.URL_ADDDRESS+'/deleteItem');
+  }
+
 }
