02d2c0dc64b3ebfff56f0d7882b838270f55bfbc
MohamadFazuan <fazuanboy@gmail.com>
2023-12-10 23:11:40 +0800

    updated: main component

--- timesheet_management/src/app/app.component.ts
+++ timesheet_management/src/app/app.component.ts
@@ -1,14 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-
-export interface Project {
-  project: string,
-  task: string,
-  assignedTo: string, 
-  from: string,
-  to: string,
-  status: string
-};
+import { MatDialog } from '@angular/material/dialog';
+import { AppService } from './app.service';
+import { Project } from '../../interface/interface';
+import { NewComponent } from './new/new.component';
 
 /**
  * @title Table with filtering
@@ -19,12 +14,19 @@ export interface Project {
   templateUrl: './app.component.html',
 })
 
-export class AppComponent {
-  displayedColumns = ['position', 'name', 'weight', 'symbol', 'update', 'delete'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+export class AppComponent implements OnInit {
+  displayedColumns = ['project', 'task', 'assignedTo', 'from', 'to', 'status', 'update', 'delete'];
+  dataSource = new MatTableDataSource<Project>();
+  dataArray: any;
 
-  ngOnInit(){
-    
+  constructor(private appService: AppService, public dialog: MatDialog) {
+  }
+
+  ngOnInit() {
+    this.appService.readAll().subscribe((res) => {
+      this.dataArray = res;
+      this.dataSource = new MatTableDataSource<Project>(this.dataArray)
+    });
   }
 
   applyFilter(event: Event) {
@@ -34,43 +36,24 @@ export class AppComponent {
     this.dataSource.filter = filterValue;
   }
 
-  edit(element: any){
+  edit(element: any) {
     console.log(element);
-    
+
   }
 
-  delete(element: any){
+  delete(element: any) {
     console.log(element);
-    
+
+  }
+
+  openDialog(){
+    const dialogRef = this.dialog.open(NewComponent, {
+      width: '1000px',
+      height: '1000px',
+    });
+
+    dialogRef.afterClosed().subscribe((res)=>{
+      console.log('The dialog closed');
+    });
   }
 }
-
-export interface Element {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: Element[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
-];
