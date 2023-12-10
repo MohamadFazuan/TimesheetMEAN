2030fb678960def2417b9814b42df3e901ac9a40
MohamadFazuan <fazuanboy@gmail.com>
2023-12-10 23:10:07 +0800

    added: added modal component

--- timesheet_management/src/app/new/new.component.ts
+++ timesheet_management/src/app/new/new.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './new.component.css'
 })
 export class NewComponent {
+  form = new FormGroup({
+    project: new FormControl('',[Validators.required]),
+    task: new FormControl('', [Validators.required]),
+    assignedTo: new FormControl('', [Validators.required]),
+    from: new FormControl('', [Validators.required]),
+    to: new FormControl('', [Validators.required]),
+    status: new FormControl('', [Validators.required])
+  })
 
+  constructor() {
+
+  }
+
+  onSubmit() {
+
+  }
+
+  onClose(){
+    
+  }
+
+  myFilter = (d: Date | null): boolean => {
+    const day = (d || new Date()).getDay();
+    // Prevent Saturday and Sunday from being selected.
+    return day !== 0 && day !== 6;
+  };
 }
